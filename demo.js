6bd1d717fa06e9465699640248dbedd4f39302de
星光 <32668397+ftb2010@users.noreply.github.com>
2021-09-08 17:13:17 +0800

    Update demo.js

--- demo.js
+++ demo.js
@@ -6,7 +6,7 @@ import { Table } from 'antd';
                         pagination={false}
                         scroll={{ y: list && list.length > 5 ? 180 : false }}
                         expandedRowRender={this.expandedRowRender}
-                        /*手动实现动态处理expandIcon
+                        /*手动实现动态处理expandIcon//有助于理解原理，不推荐
                         rowKey="xxxx"
                         expandedRowKeys={this.state.expandedRowKeys}
                         //onClick事件代码
@@ -22,6 +22,7 @@ import { Table } from 'antd';
                         })
                          }*/
                         // expandRowByClick//不建议使用有bug，没有图标也可以点
+                        //----------以下是使用官方api实现，推荐使用此代码
                         expandIcon={(props)=>{
                             return (xxx?null:
                                 (<div role="button" tabindex="0" class={props.expanded?"ant-table-row-expand-icon ant-table-row-expanded"
